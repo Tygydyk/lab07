c0e45dc8c1e072ce378d9bc9ef78ff6a93261658
grisha <grisha.kromsky@mail.ru>
2018-10-08 00:11:59 +0300

    added documentation

--- README.md
+++
@@ -1,2 +0,0 @@
-[![Build Status](https://travis-ci.org/Tygydyk/lab07.svg?branch=master)](https://travis-ci.org/Tygydyk/lab07)
-[![Build Status](https://travis-ci.org/travis-ci/travis.rb.svg?branch=master)](https://travis-ci.org/travis-ci/travis.rb)
--- include/print.hpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <fstream>
-#include <iostream>
-
-void print(const std::string& text, std::ostream& out = std::cout); //function print
-void print(const std::string& text, std::ofstream& out); //function print 2(overload)
